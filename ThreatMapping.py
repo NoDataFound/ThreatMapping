a61ae8926245e9cdea748c0b7ef55a55018026b3
Corian (Cory) Kennedy <cory@darkintel.io>
2023-03-20 18:06:22 -0500

    Update ThreatMapping.py

--- ThreatMapping.py
+++ ThreatMapping.py
@@ -1,57 +1,33 @@
-from collections.abc import Iterable
-import json
-import networkx as nx
-import altair as alt
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import altair as alt
 
-def create_node_map(data):
-    G = nx.Graph()
-    for key in data:
-        if isinstance(data[key], Iterable) and not isinstance(data[key], str):
-            for value in data[key]:
-                if isinstance(value, dict):
-                    value = tuple(sorted(value.items()))
-                elif isinstance(value, list):
-                    value = frozenset(value)
-                G.add_edge(key, value)
-        elif isinstance(data[key], dict):
-            data[key] = tuple(sorted(data[key].items()))
-            G.add_edge(key, data[key])
-        elif isinstance(data[key], list):
-            data[key] = frozenset(data[key])
-            G.add_edge(key, data[key])
-        else:
-            G.add_edge(key, data[key])
-    return G
+# Define function to parse JSON file
+def parse_json(file):
+    data = pd.read_json(file)
+    return data
 
-def create_altair_chart(graph):
-    source, target = zip(*graph.edges())
-    nodes = list(set(source + target))
-    nodes_df = pd.DataFrame({'id': nodes, 'label': nodes})
-    edges_df = pd.DataFrame({'source': source, 'target': target})
-    chart = alt.Chart(edges_df).mark_circle(size=200).encode(
-        x=alt.X('source:N', axis=alt.Axis(title='Source Node')),
-        y=alt.Y('target:N', axis=alt.Axis(title='Target Node')),
-        tooltip=['source', 'target']
-    ).properties(width=600, height=600)
-    text = chart.mark_text(fontSize=20).encode(
-        text='id'
+# Define function to create dynamic visualization
+def create_chart(data):
+    chart = alt.Chart(data).mark_bar().encode(
+        x=alt.X('category:N', sort='-y'),
+        y=alt.Y('count:Q'),
+        color=alt.Color('category:N')
+    ).properties(
+        width=600,
+        height=400
     )
-    return (chart + text).interactive()
+    return chart
 
+# Define main function for Streamlit app
 def main():
-    st.title("JSON Visualizer")
-    st.write("Upload a JSON file to visualize its contents.")
-    file = st.file_uploader("Upload JSON", type=["json"])
-    if file is not None:
-        data = json.load(file)
-        graph = create_node_map(data)
-        st.subheader("Node Map")
-        st.altair_chart(create_altair_chart(graph), use_container_width=True)
-        st.subheader("Table")
-        df = pd.DataFrame(data.items(), columns=['Key', 'Value'])
-        st.dataframe(df)
+    st.title('JSON File Visualizer')
+    file = st.file_uploader('Upload JSON file', type='json')
+    if file:
+        data = parse_json(file)
+        st.write(data)
+        chart = create_chart(data)
+        st.altair_chart(chart, use_container_width=True)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
