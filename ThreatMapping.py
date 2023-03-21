342f5288062212404b041fb9e400baf0b9fd0b9d
Corian (Cory) Kennedy <cory@darkintel.io>
2023-03-21 10:28:56 -0500

    Update ThreatMapping.py

--- ThreatMapping.py
+++ ThreatMapping.py
@@ -1,71 +1,24 @@
 import streamlit as st
 import json
 import networkx as nx
-import plotly.graph_objs as go
-from plotly.subplots import make_subplots
+import matplotlib.pyplot as plt
 
+st.title('JSON Node Graph Visualizer')
+st.text('This app allows you to upload a JSON file and visualize it with a node graph')
 
-def create_graph(data):
-    # Create a NetworkX graph from the JSON data
-    G = nx.Graph()
+# Get JSON file from user
+json_file = st.file_uploader('Upload a JSON file', type = 'json')
+if json_file is not None:
+    # Load the JSON file
+    with open(json_file) as f:
+        data = json.load(f)
 
-    for node in data["nodes"]:
-        G.add_node(node["id"], label=node["label"])
+    # Generate graph from JSON data  
+    G = nx.Graph(data)
 
-    for edge in data["edges"]:
-        G.add_edge(edge["source"], edge["target"])
+    # Draw graph 
+    plt.figure(figsize=(8, 8)) 
+    nx.draw_networkx(G, with_labels=True, node_color='skyblue', node_size=1200, alpha=0.5, linewidths=5, font_size=16) 
 
-    # Create a Plotly figure from the graph
-    pos = nx.spring_layout(G)
-
-    fig = make_subplots(rows=1, cols=1)
-    for edge in G.edges():
-        x0, y0 = pos[edge[0]]
-        x1, y1 = pos[edge[1]]
-        fig.add_trace(
-            go.Scatter(x=[x0, x1, None], y=[y0, y1, None], mode="lines"),
-            row=1,
-            col=1,
-        )
-
-    node_x = []
-    node_y = []
-    node_label = []
-    for node in G.nodes():
-        x, y = pos[node]
-        node_x.append(x)
-        node_y.append(y)
-        node_label.append(G.nodes[node]["label"])
-
-    fig.add_trace(
-        go.Scatter(x=node_x, y=node_y, mode="markers+text", text=node_label),
-        row=1,
-        col=1,
-    )
-
-    fig.update_layout(
-        showlegend=False,
-        height=600,
-        width=800,
-        margin=dict(l=0, r=0, b=0, t=0),
-        hovermode="closest",
-        plot_bgcolor="white",
-    )
-
-    return fig
-
-
-# Define the Streamlit app
-st.title("JSON Node Graph")
-uploaded_file = st.file_uploader("Choose a JSON file", type="json")
-
-if uploaded_file is not None:
-    # Load the JSON data
-    data = json.load(uploaded_file)
-
-    # Create the node graph
-    fig = create_graph(data)
-
-    # Show the node graph
-    st.plotly_chart(fig)
-
+    # Show graph in Streamlit app 
+    st.pyplot()
