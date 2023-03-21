d603049ac1bda508499450793811186ba95f9ece
Corian (Cory) Kennedy <cory@darkintel.io>
2023-03-21 10:22:37 -0500

    Update ThreatMapping.py

--- ThreatMapping.py
+++ ThreatMapping.py
@@ -1,32 +1,71 @@
-import json
 import streamlit as st
+import json
 import networkx as nx
-import matplotlib.pyplot as plt
-from networkx.readwrite import json_graph
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 
-# Set page title and description
-st.set_page_config(page_title="JSON Node Graph", page_icon=":bar_chart:", layout="wide")
+
+def create_graph(data):
+    # Create a NetworkX graph from the JSON data
+    G = nx.Graph()
+
+    for node in data["nodes"]:
+        G.add_node(node["id"], label=node["label"])
+
+    for edge in data["edges"]:
+        G.add_edge(edge["source"], edge["target"])
+
+    # Create a Plotly figure from the graph
+    pos = nx.spring_layout(G)
+
+    fig = make_subplots(rows=1, cols=1)
+    for edge in G.edges():
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
+        fig.add_trace(
+            go.Scatter(x=[x0, x1, None], y=[y0, y1, None], mode="lines"),
+            row=1,
+            col=1,
+        )
+
+    node_x = []
+    node_y = []
+    node_label = []
+    for node in G.nodes():
+        x, y = pos[node]
+        node_x.append(x)
+        node_y.append(y)
+        node_label.append(G.nodes[node]["label"])
+
+    fig.add_trace(
+        go.Scatter(x=node_x, y=node_y, mode="markers+text", text=node_label),
+        row=1,
+        col=1,
+    )
+
+    fig.update_layout(
+        showlegend=False,
+        height=600,
+        width=800,
+        margin=dict(l=0, r=0, b=0, t=0),
+        hovermode="closest",
+        plot_bgcolor="white",
+    )
+
+    return fig
+
+
+# Define the Streamlit app
 st.title("JSON Node Graph")
-
-# Upload JSON file
 uploaded_file = st.file_uploader("Choose a JSON file", type="json")
 
 if uploaded_file is not None:
-    # Load JSON data
+    # Load the JSON data
     data = json.load(uploaded_file)
 
-    # Create NetworkX graph from JSON data
-    graph = json_graph.node_link_graph(data)
+    # Create the node graph
+    fig = create_graph(data)
 
-    # Draw graph using NetworkX and Matplotlib
-    pos = nx.spring_layout(graph)
-    nx.draw_networkx_nodes(graph, pos, node_size=100, node_color='r', alpha=0.8)
-    nx.draw_networkx_edges(graph, pos, alpha=0.5)
-    nx.draw_networkx_labels(graph, pos, font_size=8, font_family='sans-serif')
-    plt.axis('off')
-    st.pyplot()
-
-    # Show raw JSON data
-    st.subheader("Raw JSON data")
-    st.json(data)
+    # Show the node graph
+    st.plotly_chart(fig)
 
