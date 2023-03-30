fa398beec0ea16d6b2d965eb5d2851847d1870ab
Corian (Cory) Kennedy <cory@darkintel.io>
2023-03-30 09:24:57 -0500

    Update ThreatMapping.py

--- ThreatMapping.py
+++ ThreatMapping.py
@@ -33,9 +33,15 @@ def build_graph(data):
 
 def visualize_graph(G):
     dot_str = nx.drawing.nx_pydot.to_pydot(G).to_string()
-    graph = pydot.graph_from_dot_data(dot_str)
-    graph[0].set('dpi', '300')
-    st.graphviz_chart(graph[0].to_string())
+    st.write("DOT string:")
+    st.write(dot_str)
+    try:
+        graph = pydot.graph_from_dot_data(dot_str)
+        graph[0].set('dpi', '300')
+        st.graphviz_chart(graph[0].to_string())
+    except Exception as e:
+        st.write("Error while visualizing graph:", e)
+
 
 
 def main():
